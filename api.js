c557ff302833ea67e610276955f4c2150370d42e
alkouny2001 <alimah2001@hotmail.com>
2026-05-19 00:44:47 +0300

    redeploy

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true, // ← sends cookies automatically
+  withCredentials: true, 
 })
 
 const getAccessToken = () => localStorage.getItem('access_token')
